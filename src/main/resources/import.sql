826b9d948844b8506fb3332187b6a57c527794e7
jsovalles <jsovalles95@gmail.com>
2020-08-22 10:10:58 -0500

    h2 database example

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+/* Populate table employees  */
+INSERT INTO employees (full_name, function, boss_id) VALUES('Pedro Gonzalez', 'Desarrollador Junior', '2');
+INSERT INTO employees (full_name, function, boss_id) VALUES('Javier Espinoza', 'Jefe de Proyectos', '1');
+INSERT INTO employees (full_name, function, boss_id) VALUES('Karen Suarez', 'Gerente General', null);
+INSERT INTO employees (full_name, function, boss_id) VALUES('Andres Jimenez', 'Desarrollador Senior', '1');
+INSERT INTO employees (full_name, function, boss_id) VALUES('Angelica Moreno', 'Desarrollador iOS', '1');
+
+/* Populate table bosses */
+INSERT INTO bosses (full_name) VALUES('Karen Suarez');
+INSERT INTO bosses (full_name) VALUES('Javier Espinoza');
