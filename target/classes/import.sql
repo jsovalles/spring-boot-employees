3392cb3df17e3dff4a32e01abcb3c00d70609e49
jsovalles <jsovalles95@gmail.com>
2020-08-22 21:30:07 -0500

    target

---
+++ target/classes/import.sql
@@ -0,0 +1,6 @@
+/* Populate table employees  */
+INSERT INTO employees (boss_id, full_name, function_role) VALUES(null, 'Karen Suarez', 'Gerente General');
+INSERT INTO employees (boss_id, full_name, function_role) VALUES(1, 'Javier Espinoza', 'Jefe de Proyectos');
+INSERT INTO employees (boss_id, full_name, function_role) VALUES(2, 'Pedro Gonzalez', 'Desarrollador Junior');
+INSERT INTO employees (boss_id, full_name, function_role) VALUES(2, 'Andres Jimenez', 'Desarrollador Senior');
+INSERT INTO employees (boss_id, full_name, function_role) VALUES(2, 'Angelica Moreno', 'Desarrollador iOS');
